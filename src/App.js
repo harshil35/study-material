f2043775674afc829a04140e80ad93fe74407da1
harshil35 <harshilap2004@gmail.com>
2022-07-31 18:07:16 +0530

    hashrouter method

--- src/App.js
+++ src/App.js
@@ -12,127 +12,127 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-          <Route path="/study-material/mail" element={<Mail/>}/>
-          <Route path="/study-material" element={<HomePage/>}/>
-          <Route path="/study-material/EM" element={<EM/>}/>
-          <Route path="/study-material/EM/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/EM/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/EM/Books" element={<ShowLinks/>}/>
+          <Route path="/mail" element={<Mail/>}/>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/EM" element={<EM/>}/>
+          <Route path="/EM/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/EM/Notes" element={<ShowLinks/>}/>
+          <Route path="/EM/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/M1" element={<EM/>}/>
-          <Route path="/study-material/M1/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/M1/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/M1/Books" element={<ShowLinks/>}/>
+          <Route path="/M1" element={<EM/>}/>
+          <Route path="/M1/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/M1/Notes" element={<ShowLinks/>}/>
+          <Route path="/M1/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/EG" element={<EM/>}/>
-          <Route path="/study-material/EG/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/EG/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/EG/Books" element={<ShowLinks/>}/>
+          <Route path="/EG" element={<EM/>}/>
+          <Route path="/EG/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/EG/Notes" element={<ShowLinks/>}/>
+          <Route path="/EG/Books" element={<ShowLinks/>}/>
        
-          <Route path="/study-material/CHEM" element={<EM/>}/>
-          <Route path="/study-material/CHEM/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/CHEM/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/CHEM/Books" element={<ShowLinks/>}/>
+          <Route path="/CHEM" element={<EM/>}/>
+          <Route path="/CHEM/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/CHEM/Notes" element={<ShowLinks/>}/>
+          <Route path="/CHEM/Books" element={<ShowLinks/>}/>
 
           <Route path="/BEE" element={<EM/>}/>
           <Route path="/BEE/PastPapers" element={<ShowLinks/>}/>
           <Route path="/BEE/Notes" element={<ShowLinks/>}/>
           <Route path="/BEE/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/study-material/BME" element={<EM/>}/>
-          <Route path="/study-material/BME/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/BME/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/BME/Books" element={<ShowLinks/>}/>
+          <Route path="/BME" element={<EM/>}/>
+          <Route path="/BME/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/BME/Notes" element={<ShowLinks/>}/>
+          <Route path="/BME/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/PHY" element={<EM/>}/>
-          <Route path="/study-material/PHY/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/PHY/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/PHY/Books" element={<ShowLinks/>}/>
+          <Route path="/PHY" element={<EM/>}/>
+          <Route path="/PHY/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/PHY/Notes" element={<ShowLinks/>}/>
+          <Route path="/PHY/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/BLE" element={<EM/>}/>
-          <Route path="/study-material/BLE/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/BLE/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/BLE/Books" element={<ShowLinks/>}/>
+          <Route path="/BLE" element={<EM/>}/>
+          <Route path="/BLE/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/BLE/Notes" element={<ShowLinks/>}/>
+          <Route path="/BLE/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/ENG" element={<EM/>}/>
-          <Route path="/study-material/ENG/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/ENG/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/ENG/Books" element={<ShowLinks/>}/>
+          <Route path="/ENG" element={<EM/>}/>
+          <Route path="/ENG/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/ENG/Notes" element={<ShowLinks/>}/>
+          <Route path="/ENG/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/ESE" element={<EM/>}/>
-          <Route path="/study-material/ESE/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/ESE/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/ESE/Books" element={<ShowLinks/>}/>
+          <Route path="/ESE" element={<EM/>}/>
+          <Route path="/ESE/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/ESE/Notes" element={<ShowLinks/>}/>
+          <Route path="/ESE/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/PSCP" element={<EM/>}/>
-          <Route path="/study-material/PSCP/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/PSCP/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/PSCP/Books" element={<ShowLinks/>}/>
+          <Route path="/PSCP" element={<EM/>}/>
+          <Route path="/PSCP/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/PSCP/Notes" element={<ShowLinks/>}/>
+          <Route path="/PSCP/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/M2" element={<EM/>}/>
-          <Route path="/study-material/M2/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/M2/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/M2/Books" element={<ShowLinks/>}/>
+          <Route path="/M2" element={<EM/>}/>
+          <Route path="/M2/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/M2/Notes" element={<ShowLinks/>}/>
+          <Route path="/M2/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/DLD" element={<EM/>}/>
-          <Route path="/study-material/DLD/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/DLD/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/DLD/Books" element={<ShowLinks/>}/>
+          <Route path="/DLD" element={<EM/>}/>
+          <Route path="/DLD/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/DLD/Notes" element={<ShowLinks/>}/>
+          <Route path="/DLD/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/DM" element={<EM/>}/>
-          <Route path="/study-material/DM/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/DM/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/DM/Books" element={<ShowLinks/>}/>
+          <Route path="/DM" element={<EM/>}/>
+          <Route path="/DM/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/DM/Notes" element={<ShowLinks/>}/>
+          <Route path="/DM/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/DSA" element={<EM/>}/>
-          <Route path="/study-material/DSA/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/DSA/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/DSA/Books" element={<ShowLinks/>}/>
+          <Route path="/DSA" element={<EM/>}/>
+          <Route path="/DSA/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/DSA/Notes" element={<ShowLinks/>}/>
+          <Route path="/DSA/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/NA" element={<EM/>}/>
-          <Route path="/study-material/NA/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/NA/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/NA/Books" element={<ShowLinks/>}/>
+          <Route path="/NA" element={<EM/>}/>
+          <Route path="/NA/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/NA/Notes" element={<ShowLinks/>}/>
+          <Route path="/NA/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/PSQT" element={<EM/>}/>
-          <Route path="/study-material/PSQT/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/PSQT/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/PSQT/Books" element={<ShowLinks/>}/>
+          <Route path="/PSQT" element={<EM/>}/>
+          <Route path="/PSQT/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/PSQT/Notes" element={<ShowLinks/>}/>
+          <Route path="/PSQT/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/UTP" element={<EM/>}/>
-          <Route path="/study-material/UTP/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/UTP/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/UTP/Books" element={<ShowLinks/>}/>
+          <Route path="/UTP" element={<EM/>}/>
+          <Route path="/UTP/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/UTP/Notes" element={<ShowLinks/>}/>
+          <Route path="/UTP/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/DBMS" element={<EM/>}/>
-          <Route path="/study-material/DBMS/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/DBMS/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/DBMS/Books" element={<ShowLinks/>}/>
+          <Route path="/DBMS" element={<EM/>}/>
+          <Route path="/DBMS/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/DBMS/Notes" element={<ShowLinks/>}/>
+          <Route path="/DBMS/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/MOT" element={<EM/>}/>
-          <Route path="/study-material/MOT/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/MOT/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/MOT/Books" element={<ShowLinks/>}/>
+          <Route path="/MOT" element={<EM/>}/>
+          <Route path="/MOT/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/MOT/Notes" element={<ShowLinks/>}/>
+          <Route path="/MOT/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/ICA" element={<EM/>}/>
-          <Route path="/study-material/ICA/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/ICA/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/ICA/Books" element={<ShowLinks/>}/>
+          <Route path="/ICA" element={<EM/>}/>
+          <Route path="/ICA/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/ICA/Notes" element={<ShowLinks/>}/>
+          <Route path="/ICA/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/OOP" element={<EM/>}/>
-          <Route path="/study-material/OOP/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/OOP/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/OOP/Books" element={<ShowLinks/>}/>
+          <Route path="/OOP" element={<EM/>}/>
+          <Route path="/OOP/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/OOP/Notes" element={<ShowLinks/>}/>
+          <Route path="/OOP/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/CA" element={<EM/>}/>
-          <Route path="/study-material/CA/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/CA/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/CA/Books" element={<ShowLinks/>}/>
+          <Route path="/CA" element={<EM/>}/>
+          <Route path="/CA/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/CA/Notes" element={<ShowLinks/>}/>
+          <Route path="/CA/Books" element={<ShowLinks/>}/>
 
-          <Route path="/study-material/DAA" element={<EM/>}/>
-          <Route path="/study-material/DAA/PastPapers" element={<ShowLinks/>}/>
-          <Route path="/study-material/DAA/Notes" element={<ShowLinks/>}/>
-          <Route path="/study-material/DAA/Books" element={<ShowLinks/>}/>
+          <Route path="/DAA" element={<EM/>}/>
+          <Route path="/DAA/PastPapers" element={<ShowLinks/>}/>
+          <Route path="/DAA/Notes" element={<ShowLinks/>}/>
+          <Route path="/DAA/Books" element={<ShowLinks/>}/>
 
       </Routes>
     </HashRouter>
